0a8f05e11de27fc8ab0c95ae6549c147cbac290c
Brian Osborn <bosborn@gmail.com>
2015-07-07 09:35:14 -0600

    icon constants

---
+++ geopackage-sample-ios/util/GPKGSConstants.h
@@ -0,0 +1,24 @@
+//
+//  GPKGSConstants.h
+//  geopackage-sample-ios
+//
+//  Created by Brian Osborn on 7/7/15.
+//  Copyright (c) 2015 NGA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const GPKGS_ICON_GEOMETRY;
+extern NSString * const GPKGS_ICON_POINT;
+extern NSString * const GPKGS_ICON_LINESTRING;
+extern NSString * const GPKGS_ICON_POLYGON;
+extern NSString * const GPKGS_ICON_TILES;
+extern NSString * const GPKGS_ICON_PAINT;
+extern NSString * const GPKGS_ICON_UP;
+extern NSString * const GPKGS_ICON_DOWN;
+extern NSString * const GPKGS_CELL_DATABASE;
+extern NSString * const GPKGS_CELL_TABLE;
+
+@interface GPKGSConstants : NSObject
+
+@end
---
+++ geopackage-sample-ios/util/GPKGSConstants.m
@@ -0,0 +1,24 @@
+//
+//  GPKGSConstants.m
+//  geopackage-sample-ios
+//
+//  Created by Brian Osborn on 7/7/15.
+//  Copyright (c) 2015 NGA. All rights reserved.
+//
+
+#import "GPKGSConstants.h"
+
+NSString * const GPKGS_ICON_GEOMETRY = @"Geometry";
+NSString * const GPKGS_ICON_POINT = @"Point";
+NSString * const GPKGS_ICON_LINESTRING = @"Linestring";
+NSString * const GPKGS_ICON_POLYGON = @"Polygon";
+NSString * const GPKGS_ICON_TILES = @"Tiles";
+NSString * const GPKGS_ICON_PAINT = @"Paint";
+NSString * const GPKGS_ICON_UP = @"Up";
+NSString * const GPKGS_ICON_DOWN = @"Down";
+NSString * const GPKGS_CELL_DATABASE = @"GeoPackageDatabaseCell";
+NSString * const GPKGS_CELL_TABLE = @"GeoPackageTableCell";
+
+@implementation GPKGSConstants
+
+@end
