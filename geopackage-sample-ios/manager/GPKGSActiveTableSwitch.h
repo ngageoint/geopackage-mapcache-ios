1c32980b66bfce7834baa0c91ce33ed0df8dae72
Brian Osborn <bosborn@gmail.com>
2015-07-07 09:21:10 -0600

    files directory refactor

---
+++ geopackage-sample-ios/manager/GPKGSActiveTableSwitch.h
@@ -0,0 +1,13 @@
+//
+//  GPKGSActiveTableSwitch.h
+//  geopackage-sample-ios
+//
+//  Created by Brian Osborn on 7/7/15.
+//  Copyright (c) 2015 NGA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPKGSActiveTableSwitch : UISwitch
+
+@end
---
+++ geopackage-sample-ios/manager/GPKGSActiveTableSwitch.m
@@ -0,0 +1,21 @@
+//
+//  GPKGSActiveTableSwitch.m
+//  geopackage-sample-ios
+//
+//  Created by Brian Osborn on 7/7/15.
+//  Copyright (c) 2015 NGA. All rights reserved.
+//
+
+#import "GPKGSActiveTableSwitch.h"
+
+@implementation GPKGSActiveTableSwitch
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
