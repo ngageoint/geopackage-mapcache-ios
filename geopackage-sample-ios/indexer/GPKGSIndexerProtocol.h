b9f70ce101d62c6abff74d844b65f104cbcd1094
Brian Osborn <bosborn@gmail.com>
2015-07-20 07:53:14 -0600

    feature indexer

---
+++ geopackage-sample-ios/indexer/GPKGSIndexerProtocol.h
@@ -0,0 +1,22 @@
+//
+//  GPKGSIndexerProtocol.h
+//  geopackage-sample-ios
+//
+//  Created by Brian Osborn on 7/15/15.
+//  Copyright (c) 2015 NGA. All rights reserved.
+//
+
+#ifndef geopackage_sample_ios_GPKGSIndexerProtocol_h
+#define geopackage_sample_ios_GPKGSIndexerProtocol_h
+
+@protocol GPKGSIndexerProtocol <NSObject>
+
+-(void) onIndexerCanceled: (NSString *) result;
+
+-(void) onIndexerFailure: (NSString *) result;
+
+-(void) onIndexerCompleted: (NSString *) result;
+
+@end
+
+#endif
