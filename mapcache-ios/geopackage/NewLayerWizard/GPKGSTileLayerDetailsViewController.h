1343205b6d3d8b1be419fdfd38510f7432d4e8cd
Tyler Burgett <tyler.burgett@gmail.com>
2018-01-10 17:42:51 -0700

    More in the new tile layer wizard. More cell types created.

---
+++ mapcache-ios/geopackage/NewLayerWizard/GPKGSTileLayerDetailsViewController.h
@@ -0,0 +1,17 @@
+//
+//  GPKGSTileLayerDetailsViewController.h
+//  mapcache-ios
+//
+//  Created by Tyler Burgett on 1/9/18.
+//  Copyright © 2018 NGA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GPKGSSectionTitleCell.h"
+#import "GPKGSFieldWithTitleCell.h"
+#import "GPKGSDesctiptionCell.h"
+#import "GPKGSButtonCell.h"
+
+@interface GPKGSTileLayerDetailsViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
---
+++ mapcache-ios/geopackage/cells/GPKGSFieldWithTitleCell.m
@@ -0,0 +1,25 @@
+//
+//  GPKGSFieldWithTitleCellTableViewCell.m
+//  mapcache-ios
+//
+//  Created by Tyler Burgett on 1/9/18.
+//  Copyright © 2018 NGA. All rights reserved.
+//
+
+#import "GPKGSFieldWithTitleCell.h"
+
+@implementation GPKGSFieldWithTitleCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    [self setBackgroundColor:[UIColor clearColor]];
+    [self setSelectionStyle:UITableViewCellSelectionStyleNone];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
