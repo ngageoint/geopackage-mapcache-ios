084273b2bc9a186ead0ce114546b99b5daad02c5
Tyler Burgett <tyler.burgett@gmail.com>
2018-02-15 09:51:55 -0700

    Bounding box wizard step

---
+++ mapcache-ios/geopackage/NewLayerWizard/MCZoomAndQualityViewController.h
@@ -0,0 +1,17 @@
+//
+//  MCZoomAndQualityViewController.h
+//  mapcache-ios
+//
+//  Created by Tyler Burgett on 2/7/18.
+//  Copyright © 2018 NGA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GPKGSSectionTitleCell.h"
+#import "GPKGSFieldWithTitleCell.h"
+#import "GPKGSSegmentedControlCell.h"
+#import "GPKGSButtonCell.h"
+
+@interface MCZoomAndQualityViewController : UITableViewController
+
+@end
